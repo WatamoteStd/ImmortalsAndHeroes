6f321cd1919b69065765800669ffd783bcb15184
watashiwa <watashiwadesu121@gmail.com>
2026-07-20 11:03:16 +0300

    ClientEntity, RegionClient added.

---
+++ Client/Scripts/Entities/EntityClient.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class EntityClient : CharacterBody3D
+{
+	
+	public enum EntityState { Idle, Move, Chase, Attack, Cast, Dead}
+	public EntityState CurrentState = EntityState.Idle;
+
+	public long Id {get; set;}
+	public int Health {get; set;}
+	public float CurrentSpeed {get; set;}
+
+	public Vector3 TargetPosition {get; set;}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+		switch (CurrentState)
+		{
+			
+			case EntityState.Idle:
+
+			break;
+
+			case EntityState.Move:
+				{
+					
+
+
+				}
+			break;
+
+		}
+
+	}
+
+	public virtual void MoveToPosition(Vector3 position)
+	{
+		
+		
+
+	}
+
+
+
+}
--- Client/Scripts/World/RegionClient.cs
+++ Client/Scripts/World/RegionClient.cs
@@ -4,10 +4,28 @@ using System;
 public partial class RegionClient : Node3D
 {
 
+	public readonly Godot.Collections.Dictionary<long, EntityClient> _regionEntities = new();
+	[Export] private PackedScene _modelPrefab;
+ 
 	public override void _Ready()
 	{
 		
-		NetworkUdpClient.Instance.SendEnterTheWorld();
+		NetworkUdpClient.Instance.SendEnterTheWorld(); // UDP REQUEST TO SERVER
+
+	}
+	
+
+	public void AddEntity(long id, Vector3 startPosition)
+	{
+
+		if (_regionEntities.ContainsKey(id)) return;
+		
+		var entity = _modelPrefab.Instantiate<EntityClient>();
+		entity.Id = id;
+		entity.GlobalPosition = startPosition;
+		AddChild(entity);
+
+		_regionEntities.Add(id, entity);
 
 	}
 
